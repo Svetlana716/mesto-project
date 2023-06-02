e4d788c40a993b724c70d1a48cf1264345d17c12
Svetlana Sunagatullina <Ssunagatullina@mail.ru>
2023-06-02 23:39:20 +0300

    add new cards using only JS

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const container = document.querySelector('.container');
 const popup = document.querySelector('.popup');
-const editButton = document.querySelector('.profile__edit-button');
+const editButton = container.querySelector('.profile__edit-button');
 const closeButton = popup.querySelector('.popup__close-button');
 const submitButton = popup.querySelector('.form__submit-button');
+const addButton = container.querySelector('.profile__add-button');
 
 function openPopup(evt) {
   evt.preventDefault();
@@ -9,6 +11,8 @@ function openPopup(evt) {
 }
 
 editButton.addEventListener('click', openPopup);
+
+addButton.addEventListener('click', openPopup);
 
 function closePopup(evt) {
   evt.preventDefault();
@@ -34,3 +38,41 @@ closePopup(evt)
 };
 
 formElement.addEventListener('submit', handleFormSubmit);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function(element){
+  const cardsList = container.querySelector('.cards__list');
+  const templateCard = document.querySelector('#card').content;
+  const cardElement = templateCard.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__title').textContent = element.name;
+
+  cardsList.append(cardElement);
+});
