b7a43b907cfcbf46762172f165c53abfcdda83c5
Svetlana Sunagatullina <Ssunagatullina@mail.ru>
2023-08-01 11:51:41 +0300

    изменила класс карточек

--- src/components/api.js
+++ src/components/api.js
@@ -11,119 +11,60 @@ export default class Api {
     return res.json();
   }
 
-  //сделал небольшой рефакторинг
-  //чтобы не дублировать в запросах fetch и then, создал приватный метод и его уже вызываю в запросах к серверу
-  /*_request(endpoint, options) {
+  _request(endpoint, options) {
     return fetch(`${this._baseUrl}/${endpoint}`, options).then(this._checkResponse);
-  }*/
+  }
 
   getUserInfo() {
-    return fetch(`${this._baseUrl}/users/me`, {
-      headers: this._headers,
-    }).then(this._checkResponse);
+    return _request(users/me, { headers: this._headers });
   }
-
-  /*getUserInfo() {
-    return request(`/users/me`, { headers: this._headers });
-  }*/
 
   getInitialCards() {
-    return fetch(`${this._baseUrl}/cards`, {
-      headers: this._headers,
-    }).then(this._checkResponse);
+    return _request(cards, { headers: this._headers });
   }
-
-  /*getInitialCards() {
-    return request(`/cards`, { headers: this._headers });
-  }*/
 
   editProfile(name, about) {
-    return fetch(`${this._baseUrl}/users/me`, {
-      method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify(name, about),
-    }).then(this._checkResponse);
-  }
-
-  /*editProfile(name, about) {
-    return request(`/users/me`, {
+    return _request(users/me, {
       method: "PATCH",
       body: JSON.stringify(name, about),
       headers: this._headers,
     });
-  }*/
+  }
 
   editAvatar(avatar) {
-    return fetch(`${this._baseUrl}/users/me/avatar`, {
-      method: "PATCH",
-      headers: this._headers,
-      body: JSON.stringify(avatar),
-    }).then(this._checkResponse);
-  }
-
-  /*editAvatar(avatar) {
-    return request(`/users/me/avatar`, {
+    return _request(users/me/avatar, {
       method: "PATCH",
       headers: this._headers,
       body: JSON.stringify(avatar),
     });
-  }*/
+  }
 
   postNewCard(name, link) {
-    return fetch(`${this._baseUrl}/cards`, {
-      method: "POST",
-      headers: this._headers,
-      body: JSON.stringify(name, link),
-    }).then(this._checkResponse);
-  }
-
-  /*postNewCard(name, link) {
-    return request(`/cards`, {
+    return _request(cards, {
       method: "POST",
       headers: this._headers,
       body: JSON.stringify(name, link),
     });
-  }*/
+  }
 
   deleteCard(cardId) {
-    return fetch(`${this._baseUrl}/cards/${cardId}`, {
-      method: "DELETE",
-      headers: this._headers,
-    }).then(this._checkResponse);
-  }
-
-  /*deleteCard(cardId) {
-    return request(`/cards/${cardId}`, {
+    return _request(cards/`${cardId}`, {
       method: "DELETE",
       headers: this._headers,
     });
-  }*/
+  }
 
   likeCard(cardId) {
-    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
-      method: "PUT",
-      headers: this._headers,
-    }).then(this._checkResponse);
-  }
-
-  /*likeCard(cardId) {
-    return request(`/cards/likes/${cardId}`, {
+    return _request(cards/likes/`${cardId}`, {
       method: "PUT",
       headers: this._headers,
     });
-  }*/
-
-  disLikeCard(cardId) {
-    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
-      method: "delete",
-      headers: this._headers,
-    }).then(this._checkResponse);
   }
 
-  /*disLikeCard(cardId) {
-  return request(`/cards/likes/${cardId}`, {
+  disLikeCard(cardId) {
+  return _request(cards/likes/`${cardId}`, {
     method: "DELETE",
     headers: this._headers,
   });
-}*/
 }
+};
--- src/components/card.js
+++ src/components/card.js
@@ -1,24 +1,9 @@
-import {
-  popupImage,
-  popupImageTitle,
-  popupFullCardImage,
-  config,
-} from "../utils/constants.js";
-import { openPopup } from "./modal.js";
-import { checkReject } from "../utils/utils.js";
-import Api from "./Api.js";
-
-const api = new Api(config); // наверное создавать экземпляры класса будем уже в index.js, здесь только прописываем класс, поэтому все импорты наверное надо поудалять, как думаешь?
-
 export default class Card {
   constructor(
     data,
     userId,
     cardSelector,
-    handleCardClick,
-    handleDeleteCards,
-    handleDeleteLike,
-    handleAddLike
+    {handleCardClick, handleDeleteCards, handleDeleteLike, handleAddLike}
   ) {
     this._cardName = data.name;
     this._cardLink = data.link;
@@ -27,13 +12,12 @@ export default class Card {
     this._cardLikes = data.likes;
     this._userId = userId;
     this._cardSelector = cardSelector;
-    this._handleCardClick = handleCardClick; //колбэк слушателя клмка по карточ
+    this._handleCardClick = handleCardClick; //колбэк слушателя клика по карточке
     this._handleDeleteCards = handleDeleteCards; //колбэк слушателя клика урны
-    this._handleDeleteLike = handleDeleteLike; //колбэк слушат удаления лайка
+    this._handleDeleteLike = handleDeleteLike; //колбэк слушателя удаления лайка
     this._handleAddLike = handleAddLike; //колбэк слушателя добавления лайка
   }
-  // не забыть, что в HTML ищем не по селектору класса, а по селектору ID
-  //думаю, что при создании экземпляра класса Card аргумент параметра  cardSelector нужно указывать с решеткой
+
   _getCard() {
     const cardElement = document
       .querySelector(this._selector)
@@ -41,16 +25,10 @@ export default class Card {
       .cloneNode(true);
     return cardElement;
   }
-  //заменил объявление переменной через const на this._card
-  /*_getCard () {
-    this._card = document.querySelector(this._selector).content.querySelector('.card').cloneNode(true);
-    return this._card;
-  }*/
 
   //проверяем, есть ли мой лайк на карточке
-  //в конструкторе объявили this._cardLikes = data.likes; надо заменить?
-  _likeCard(data) {
-    this._isLiked = data.likes.some((like) => {
+  _likeCard() {
+    this._isLiked = this._cardLikes.some((like) => {
       return like._id === this._userId; //добавил return, чтобы вернуло результат
     });
     this._cardLikesCounter.textContent = this._cardLikes.length;
@@ -62,9 +40,8 @@ export default class Card {
   }
 
   //проверяем есть ли лайки у карточки хоть чьи-то, чтобы поставить число лайков
-  //в конструкторе объявили this._cardLikes = data.likes; надо заменить?
-  _addLikeCounter(data) {
-    if (data.likes.length > 0) {
+  _addLikeCounter() {
+    if (this._cardLikes.length > 0) {
       this._cardLikesCounter.classList.add("card__like-counter_active");
     } else {
       this._cardLikesCounter.classList.remove("card__like-counter_active");
@@ -77,7 +54,7 @@ export default class Card {
     }
   }
   //все ок
-  _deleteCards() {
+  deleteCards() {
     this._card.remove();
     this._card = null;
   }
@@ -85,31 +62,19 @@ export default class Card {
   _setEventListeners() {
     //слушатель кнопки лайка
     this._cardLikeButton.addEventListener("click", () => {
-      this._likeCards();
-      /* вместо this._likeCards() думаю сделать так:
-      так как здесь в зависимоти от состояния лайка можно отправить на сервер два рзных запроса (likeCard либо disLikeCard), то надо прпописать логику с участием двух колбэков слушателя
-
       if (this._cardLikeButton.classList.contains('card__like-counter_active')) {
         this._handleDeleteLike(this._cardId);
       } else {
         this._handleAddLike(this._cardId);
       }
-
     });
     //слушатель корзины для удаления карточки
     this._cardDeleteButton.addEventListener("click", () => {
-      this._deleteCards();
-      /*вместо  this._deleteCards() предлагаю поставить
     this._handleDeleteCards(this._cardId)
-    и в конструкторе присвоить ему значение входного параметра конструктора*/
     });
     // открытие попапа большого изображения
     this._cardImage.addEventListener("click", () => {
-      this._fullCardImage();
-      /*вместо  this._fullCardImage() предлагаю поставить
     this._handleCardClick(this._cardName, this._cardLink)
-    и в конструкторе присвоить ему значение входного параметра конструктора,
-    он кстати у тебя в параметрах конструктора уже был:)*/
     });
   }
 
@@ -123,12 +88,30 @@ export default class Card {
     this._cardImage.src = this._link;
     this._cardImage.alt = this._name;
     this._cardTitle.textContent = this._name;
-
+    this._cardLikesCounter.textContent = this._cardLikes.length;
     this._addDeleteIcon(); //проверяем мы создали карточку или нет для урны
     this._addLikeCounter(); //проверяем стоят ли лайки у карточки для счетчика
     this._likeCard(); // проверяем наш лайк есть или нет для активности лайка
-    this.this._cardLikesCounter.textContent = this._cardLikes.length; //двойной this - опечатка???
     this._setEventListeners();
     return this._card;
   }
 }
+
+///////////////////////////////////////////////////////////////
+// создание экземпляяра классса card  в index.js
+
+function createCard (data) {
+  const card = new Card(
+    data,
+    userInfo.userId, // из класса информация о пользователе
+    '#card', {
+    handleCardClick: data => popupImage.open(data), // метод из класса popupImage
+    handleCardDelete: () => {
+      card.deleteCards();
+      api.deleteCard(data._id);
+    },
+    handleAddLike: () => api.likeCard(data._id),
+    handleDeleteLike: () => api.disLikeCard(data._id)
+  });
+  return card;
+};
