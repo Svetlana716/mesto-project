9cf900bf4d0ee4019ab55f0671272c8074fc7ffc
Izvolsky <9961200@mail.ru>
2023-08-02 19:52:04 +0300

    проверил классы PopupWithForm и PopupWithImage. Есть сомнения по методу getInputValue в PopupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,48 +1,56 @@
 import Popup from "./Popup.js";
-
-export default class PopupWithForm extends Popup{
-  constructor (popupSelector, formSubmitCallback) {
+//мне кажется сюда можно добавить метод runLoading, сделать его публичным
+//как-раз будет менять состояние кнопкт сабмит у this._form
+//методу даже не нужно будет передавать аргументы - все возьмет из конструктора
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, formSubmitCallback) {
     super(popupSelector);
     this._formSubmitCallback = formSubmitCallback;
-    this._form = this._popup.querySelector('.form');
-    this._inputList = this._form.querySelectorAll('.form__input');
+    this._form = this._popup.querySelector(".form");
+    this._inputList = this._form.querySelectorAll(".form__input");
   }
-
-  _getInputValues () {
+  //получаем данные из формы
+  //пробовал в консоли браузера это реализовать, нифига не получилось
+  //пока так оставим - может заработает:)
+  _getInputValues() {
     this.formData = new FormData(this._form);
-    return this.inputs = Object.fromEntries(this.formData.entries());
+    return (this.inputs = Object.fromEntries(this.formData.entries()));
   }
-
-  _setEventListeners () {
-    super._setEventListeners ();
-    this._form.addEventListener('submit', (evt) => {
+  //метод слушателей формы - публичнй
+  //добавляем обработчик сабмита
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener("submit", (evt) => {
       evt.preventDefault();
-      this._formSubmitCallback(this._getInputValues ());
+      this._formSubmitCallback(this._getInputValues());
     });
   }
-
-  closePopup () {
-    super.closePopup ();
+  // закрытие попапа
+  //еще добавляем сброс инпутов
+  closePopup() {
+    super.closePopup();
     this._form.reset();
   }
-};
+}
 
 ////////////////////////////////////////////////////////////////////
 
 // пример создания экземпляяра классса PopupWithForm  в index.js
-
+/*
 const popupFormEditProfile = new PopupWithForm(
-  '.popup_type_edit-profile',
+  ".popup_type_edit-profile",
   (inputs) => {
-    runLoading (true, formEditProfile);
-  api
-    .editProfile(inputs.name, inputs.about)
-    .then((data) => {
-      userInfo.setUserInfo(data); //метод класса userInfo
-      popupFormEditProfile.close();
-    })
-    .catch(checkReject)
-    .finally(() => {
-      runLoading (false, formEditProfile);
-    });
-});
+    runLoading(true, formEditProfile);
+    api
+      .editProfile(inputs.name, inputs.about)
+      .then((data) => {
+        userInfo.setUserInfo(data); //метод класса userInfo
+        popupFormEditProfile.close();
+      })
+      .catch(checkReject)
+      .finally(() => {
+        runLoading(false, formEditProfile);
+      });
+  }
+);
+*/
--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,19 +1,19 @@
 import Popup from "./Popup.js";
 
-export default class PopupWithImage extends Popup{
+export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    this._popupImage = this._popup.querySelector('popup__image');
-    this._popupImageTitle = this._popup.querySelector('popup__image-title');
+    this._popupImage = this._popup.querySelector("popup__image");
+    this._popupImageTitle = this._popup.querySelector("popup__image-title");
   }
 
-  openPopup (data) {
+  openPopup(data) {
     super.openPopup();
     this._popupImage.srs = data.link;
     this._popupImage.alt = data.name;
     this._popupImageTitle.textContent = data.name;
   }
-};
+}
 ///////////////////////////////////////////////////////////
 // создание экземпляяра классса PopupWithImage  в index.js
 
