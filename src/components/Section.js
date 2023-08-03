2ccff7dabac0264a40261fc10849ab38b46eb7ad
Izvolsky <9961200@mail.ru>
2023-08-03 15:48:50 +0300

    Поправил UserInfo, Section все четко

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,18 +1,19 @@
 export default class Section {
-  constructor ({ items, renderer }, containerSelector) {
+  //круто сделала, я сам голову ломал как реализовать
+  constructor({ items, renderer }, containerSelector) {
     this._initialArray = items; // массив данных, которые нужно добавить на страницу при инициализации класса
     this._renderer = renderer; // функция, которая отвечает за создание и отрисовку данных на странице
     this._container = document.querySelector(containerSelector); // селектор контейнера, в который нужно добавлять созданные элементы
   }
-//отвечает за отрисовку всех элементов.
-//Отрисовка каждого отдельного элемента должна осуществляться функцией renderer.
-  renderItems () {
-      this._initialArray.forEach((item) => {
-        this._renderer(item);
-      })
-    }
-// принимает DOM-элемент и добавляет его в контейнер
+  //отвечает за отрисовку всех элементов.
+  //Отрисовка каждого отдельного элемента должна осуществляться функцией renderer.
+  renderItems() {
+    this._initialArray.forEach((item) => {
+      this._renderer(item);
+    });
+  }
+  // принимает DOM-элемент и добавляет его в контейнер
   addItem(element) {
     this._container.append(element);
   }
-};
+}
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,21 +1,37 @@
 export default class UserInfo {
-  constructor ({profileNameSelector, profileDescriptionSelector, profileAvatarSelector}) {
+  constructor({
+    profileNameSelector,
+    profileDescriptionSelector,
+    profileAvatarSelector,
+  }) {
     this._profileName = document.querySelector(profileNameSelector);
-    this._profileDescription = document.querySelector(profileDescriptionSelector);
+    this._profileDescription = document.querySelector(
+      profileDescriptionSelector
+    );
     this._profileAvatar = document.querySelector(profileAvatarSelector);
   }
+  //возвращает объект с данными пользователя
+  getUserInfo() {
+    //Когда данные пользователя нужно будет подставить в форму при открытии
 
-  getUserInfo() {  //Когда данные пользователя нужно будет подставить в форму при открытии
-    return { //объект с данными пользователя
-      name: this._profileName,
-      about: this._profileDescription,
-    }
+    const dataUser = {
+      //объект с данными пользователя
+      //добавил в объект avatar
+      //записываем в объект не селекторы, а значения полей этихъ селекторов
+      name: this._profileName.textContent,
+      about: this._profileDescription.textContent,
+      avatar: this._profileAvatar.src,
+    };
+    return dataUser;
   }
+  //мне кажется, что _id: id здесь лишнее
 
-  setUserInfo ({name, about, avatar, _id: id}) { //принимает новые данные пользователя, отправляет их на сервер и добавляет их на страницу
-    this._profileName = name;
-    this._profileDescription = about;
-    this._profileAvatar = avatar;
-    this._userId = id;
+  setUserInfo(data) {
+    //принимает новые данные пользователя, отправляет их на сервер и добавляет их на страницу
+    //добавляем не селектор новый, а изменяем их атрибуты textContent и src
+    this._profileName.textContent = data.name;
+    this._profileDescription.textContent = data.about;
+    this._profileAvatar.src = data.avatar;
+    //this._userId = id;
   }
 }
